d77ea8b0900a8556eb41b0e1fe6cfec3667605d3
eneugut <57046405+eneugut@users.noreply.github.com>
2023-12-29 19:45:52 -0500

    d

--- domain_print.py
+++ domain_print.py
@@ -1,20 +1,17 @@
 import whois
+import argparse
 
-domain = "dfkdjfkdjfslkjfklasjflk.com"
-domain_info = whois.whois(domain)
-print(domain_info)
+def main():
+    # Create a command-line argument parser
+    parser = argparse.ArgumentParser(description='Perform a WHOIS lookup for a domain.')
+    parser.add_argument('domain', type=str, help='Domain name to perform WHOIS lookup on')
+    args = parser.parse_args()
 
+    # Perform the WHOIS lookup
+    domain_info = whois.whois(args.domain)
 
-domain = "google.com"
-domain_info = whois.whois(domain)
-print(domain_info)
+    # Print the WHOIS information
+    print(domain_info)
 
-
-domain = "090wer0wer9w0re90e.com"
-domain_info = whois.whois(domain)
-print(domain_info)
-
-
-domain = "facebook.com"
-domain_info = whois.whois(domain)
-print(domain_info)
+if __name__ == '__main__':
+    main()
